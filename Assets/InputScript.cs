d77608c7ea718c599b437609f3edae865910a06d
gamesketches <sam.von.ehren@gmail.com>
2016-05-26 23:15:28 -0400

    Added in the animations, wrote a simple script to animate

---
+++ Assets/InputScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputScript : MonoBehaviour {
+
+	Animator theAnimator;
+	// Use this for initialization
+	void Start () {
+		theAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){ 
+			theAnimator.SetTrigger("Compression");
+			Debug.Log("compression!");
+		}
+	}
+}

68202a354ab45dad58c4d84aa64e04127ee936e4
gamesketches <sam.von.ehren@gmail.com>
2016-06-07 17:31:23 -0400

    Got some basic rhythm code in, need to switch debug loggin over to a sound or something to get the timing down

--- Assets/InputScript.cs
+++ Assets/InputScript.cs
@@ -4,16 +4,43 @@ using System.Collections;
 public class InputScript : MonoBehaviour {
 
 	Animator theAnimator;
+	private int compressionsDone;
+	int counter;
+	public int marginOfError = 3;
+	public int beatsPerSecond;
 	// Use this for initialization
 	void Start () {
 		theAnimator = GetComponent<Animator>();
+		compressionsDone = 0;
+		counter = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.Space)){ 
-			theAnimator.SetTrigger("Compression");
-			Debug.Log("compression!");
+		if(counter > 0) {
+			counter++;
+		}
+		if(counter == beatsPerSecond) {
+			Debug.Log("now");
+		}
+		if(counter > beatsPerSecond + marginOfError) {
+			counter = 0;
+			compressionsDone = 0;
+			Debug.Log("bad timing");
+		}
+		else {
+			if(Input.GetKeyDown(KeyCode.Space)){ 
+				theAnimator.SetTrigger("Compression");
+				if(counter > 0 && counter > beatsPerSecond - marginOfError) {
+					compressionsDone++;
+					if(compressionsDone >= 5) {
+						Debug.Log("he's dead jim");
+						}
+				}
+				else {
+					counter = 1;
+				}
+			}
 		}
 	}
 }

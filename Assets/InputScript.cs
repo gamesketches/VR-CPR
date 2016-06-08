26b50a238b2060da72ff1b4b0aeec99098121ace
gamesketches <sam.von.ehren@gmail.com>
2016-06-08 15:49:09 -0400

    Added in a sound for timing

--- Assets/InputScript.cs
+++ Assets/InputScript.cs
@@ -6,39 +6,45 @@ public class InputScript : MonoBehaviour {
 	Animator theAnimator;
 	private int compressionsDone;
 	int counter;
+	AudioSource audio;
+	bool success;
 	public int marginOfError = 3;
 	public int beatsPerSecond;
 	// Use this for initialization
 	void Start () {
 		theAnimator = GetComponent<Animator>();
+		audio = GetComponent<AudioSource>();
 		compressionsDone = 0;
 		counter = 0;
+		success = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if(counter > 0) {
-			counter++;
-		}
+	void FixedUpdate () {
+		counter++;
 		if(counter == beatsPerSecond) {
-			Debug.Log("now");
+			audio.Play();
 		}
 		if(counter > beatsPerSecond + marginOfError) {
 			counter = 0;
-			compressionsDone = 0;
-			Debug.Log("bad timing");
+			if(success) {
+				compressionsDone++;
+				if(compressionsDone >= 30) {
+						Debug.Log("he's dead jim");
+						}
+			}
+			else {
+				compressionsDone = 0;
+				Debug.Log("bad timing");
+			}
+			success = false;
 		}
 		else {
 			if(Input.GetKeyDown(KeyCode.Space)){ 
 				theAnimator.SetTrigger("Compression");
-				if(counter > 0 && counter > beatsPerSecond - marginOfError) {
-					compressionsDone++;
-					if(compressionsDone >= 5) {
-						Debug.Log("he's dead jim");
-						}
-				}
-				else {
-					counter = 1;
+				if(counter > beatsPerSecond - marginOfError) {
+					Debug.Log("nice");
+					success = true;
 				}
 			}
 		}

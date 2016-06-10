cc9a1cc387778c71a09a7386eb8707fbc63e4038
gamesketches <sam.von.ehren@gmail.com>
2016-06-10 17:26:28 -0400

    Added clicking sound and reticle

---
+++ Assets/ReticleDraw.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReticleDraw : MonoBehaviour {
+
+	public float rayLength = 100f;
+	private GameObject compressionReticle;
+	private Quaternion reticleRotation;
+	// Use this for initialization
+	void Start () {
+		reticleRotation = Quaternion.Euler(90, 0, 0);
+		compressionReticle = (GameObject)Instantiate(Resources.Load<GameObject>("CompressionReticle"), new Vector3(100, 100, 100), reticleRotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray ray = new Ray(transform.position, transform.forward);
+
+		if(Physics.Raycast(ray, out hit, rayLength)){
+			if(hit.collider.gameObject.tag == "torso") {
+				compressionReticle.transform.position = hit.point;
+			}
+			else {
+			Debug.Log(hit.collider.gameObject.tag);
+				compressionReticle.transform.position = new Vector3(100, 100, 100);
+			}
+		}
+		Debug.DrawRay(transform.position, transform.forward * rayLength);
+	}
+}
